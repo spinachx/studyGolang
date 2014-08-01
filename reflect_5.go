7eac7369c2426f0840576eebd91a720c6163e46e
spinachx <spinach_xu@hotmail.com>
2014-08-01 10:12:22 +0800

    Update reflect_5.go
    函数带多参的情况下,反射的调用及传值方法

--- reflect_5.go
+++ reflect_5.go
@@ -7,13 +7,13 @@ import (
 
 type T struct {
 	Name string
-	F    func()
+	F    func(i int, j string)
 }
 
 func main() {
-	t := &T{"tt", func() { fmt.Println("ff") }}
+	t := &T{"tt", func(i int, j string) { fmt.Println(i); fmt.Println(j) }}
 	fmt.Println(t.Name)
-	t.F()
+	t.F(5, "AA")
 
 	for i := 0; i < reflect.ValueOf(t).Elem().NumField(); i++ {
 		fmt.Println(i, "--",
@@ -23,7 +23,10 @@ func main() {
 			reflect.ValueOf(t).Elem().Field(i).Interface(), "--")
 		if reflect.ValueOf(t).Elem().Field(i).Type().Kind() == reflect.Func {
 			// fmt.Println("haha")
-			reflect.ValueOf(t).Elem().Field(i).Call([]reflect.Value{})
+			v := make([]reflect.Value, 2)
+			v[0] = reflect.ValueOf(5)
+			v[1] = reflect.ValueOf("AA")
+			reflect.ValueOf(t).Elem().Field(i).Call(v)
 		}
 	}
 }
